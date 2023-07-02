546076d2c2c9a06e59b7535dcdb9818d063d1925
Chalita <chalitamichael@gmail.com>
2023-07-02 19:03:48 -0300

    ajuste

--- lib/bolso_organizado_calculator.dart
+++ lib/bolso_organizado_calculator.dart
@@ -4,22 +4,37 @@ library bolso_organizado_calculator;
 
 class Calculator {
 
-  dynamic calcular(List<double> listValue) {
-
-    double totalIncome = 0;
-    double totalOutcome = 0;
-    double totalBalance = 0;
+  double calcularRenda(List<double> listValue) {
+    double resultado = 0;
 
     for(var value in listValue){
       if(value > 0){
-        totalIncome += value;
-      }else{
-        totalOutcome += value;
+        resultado += value;
       }
-
-      totalBalance += value;
     }
 
-    return {totalIncome, totalOutcome, totalBalance};
+    return resultado;
+  }
+
+  double calcularDespesa(List<double> listValue) {
+    double resultado = 0;
+
+    for(var value in listValue){
+      if(value < 0){
+        resultado += value;
+      }
+    }
+
+    return resultado;
+  }
+
+  double calcularTotal(List<double> listValue) {
+    double resultado = 0;
+
+    for(var value in listValue){
+        resultado += value;
+    }
+
+    return resultado;
   }
 }
