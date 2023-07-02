e2e746be49caad0c3c43833f48a11bd678931198
Chalita <chalitamichael@gmail.com>
2023-07-02 18:45:11 -0300

    second commit

--- lib/bolso_organizado_calculator.dart
+++ lib/bolso_organizado_calculator.dart
@@ -1,14 +1,24 @@
 library bolso_organizado_calculator;
 
+import 'package:bolso_organizado_calculator/models/transaction_model.dart';
+
 class Calculator {
 
-  double calcular(List<double> listValue){
-    double resultado = 0;
+  dynamic calcular(List<TransactionModel> listTransactionModel) {
+    double totalIncome = 0;
+    double totalOutcome = 0;
+    double totalBalance = 0;
 
-    listValue.forEach((value) {
-      resultado += value;
-    });
+    for(var transactionModel in listTransactionModel){
+      if(transactionModel.value > 0){
+        totalIncome += transactionModel.value;
+      }else{
+        totalOutcome += transactionModel.value;
+      }
 
-    return resultado;
+      totalBalance += transactionModel.value;
+    }
+
+    return {totalIncome, totalOutcome, totalBalance};
   }
 }
